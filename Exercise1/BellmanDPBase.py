13ddd55a827e900ba7ed07fd36916d4921ee3e9b
dzhv <dz.vysniauskas@gmail.com>
2019-02-14 20:02:20 +0000

    Initial commit

---
+++ Exercise1/BellmanDPBase.py
@@ -0,0 +1,19 @@
+from MDP import MDP
+
+class BellmanDPSolver(object):
+	def __init__(self):
+		self.MDP = MDP()
+
+	def initVs(self):
+
+	def BellmanUpdate(self):
+
+		raise NotImplementedError
+		
+if __name__ == '__main__':
+	solution = BellmanDPSolver()
+	for i in range(20000):
+		values, policy = solution.BellmanUpdate()
+	print("Values : ", values)
+	print("Policy : ", policy)
+

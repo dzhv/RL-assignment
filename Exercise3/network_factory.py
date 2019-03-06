1809489b879ce7d4c16d23bef40a2eb8abfae797
dzhv <dz.vysniauskas@gmail.com>
2019-03-06 23:45:02 +0000

    fixing network imput

--- Exercise3/network_factory.py
+++ Exercise3/network_factory.py
@@ -2,7 +2,8 @@ from Networks import ValueNetwork
 
 #input_d = 72    # for Low Level Features with action
 # input_d = 19	# for High Level Features with action 
-input_d = 15
+# input_d = 15	# for High Level Features without action 
+input_d = 68    # for Low Level Features with action
 actions_n = 4
 
 def create_network():

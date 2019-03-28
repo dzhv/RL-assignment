2ffe3ee47ed5198507fff4e9c13c9c302e48da48
dzhv <dz.vysniauskas@gmail.com>
2019-03-28 23:06:09 +0000

    fixing misspelling in run sh

--- Exercise3/run.sh
+++ Exercise3/run.sh
@@ -1,7 +1,7 @@
 killall -9 rcssserver
 sleep 5
 
-python main.py --experiment "exp9" --lr 0.000025 --discountFactor 0.99 --n_workers 6 --num_layers 3
+python main.py --experiment "exp9" --lr 0.000025 --discountFactor 0.99 --n_workers 6 --num_layers 3 --hidden_size 25
 
 sleep 5
 rm -r log
@@ -10,7 +10,7 @@ sleep 10
 killall -9 rcssserver
 sleep 3
 
-python main.py --experiment "exp10" --lr 0.000025 --discountFactor 0.99 --n_workers 6 --num_layers 3 --hiden_size 40
+python main.py --experiment "exp10" --lr 0.000025 --discountFactor 0.99 --n_workers 6 --num_layers 3 --hidden_size 40
 
 sleep 5
 rm -r log
@@ -19,4 +19,4 @@ sleep 10
 killall -9 rcssserver
 sleep 3
 
-python main.py --experiment "exp11" --lr 0.000015 --discountFactor 0.99 --n_workers 6 --num_layers 3 --hiden_size 40
+python main.py --experiment "exp11" --lr 0.000015 --discountFactor 0.99 --n_workers 6 --num_layers 3 --hidden_size 40
